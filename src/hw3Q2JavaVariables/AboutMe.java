ae5450f264ad28b79fbb533913713a2a2aaa12a0
SubornaDas <subornadasqa@gmail.com>
2022-02-02 00:59:32 -0500

    02022022 12.59am

--- src/hw3Q2JavaVariables/AboutMe.java
+++ src/hw3Q2JavaVariables/AboutMe.java
@@ -1,6 +1,5 @@
 package hw3Q2JavaVariables;
 
-/* Create a package name "hw3Q2JavaVariables" inside the Home work project, Create a Class name "AboutMe". Declare only one type of variable(so you know declare vs initialize), and initialize String and other 8 types of variable (you can give a different name from mine). Instantiate AboutMe class under main method and call few variables by object (you can use String concatenation to make it more meaningful outcome). Give a single line comment where a variable is declared and initialized. Follow indentation by Organizing the code and paste below. [Mark distribution (80) : using variable --20, instantiate class - 20, call variables by object and print a meaningful outcome 20, comment for variable is declared and initialized - 10, organize code and other-- 10]*/
 public class AboutMe {
 //	int type of variable is declared, not initialized
 	public int age;
@@ -16,14 +15,12 @@ public class AboutMe {
 	public boolean deliveryMethod = false;
 
 	public static void main(String[] args) {
-//	Now we will instantiate the class
+//	Aboutme  class is instantiated
 		AboutMe aboutMe = new AboutMe();
 		System.out.println("My business name: " + aboutMe.onlineBusiness);
 		System.out.println("\tAddress: " + aboutMe.houseNumber + " Hillside drive, Zipcode " + aboutMe.zipcode);
 		System.out.println("\tOnly method of delivery is online: " + aboutMe.deliveryMethod
 				+ ", Estimated Yearly incvestment around " + aboutMe.yearlyInvestment);
 		System.out.println("\tFor more info contact number: " + aboutMe.phoneNumber);
-
 	}
-
 }
